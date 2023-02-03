b931a1741213e1288a96c53b531f59d3d3fe0936
NKleinenkuhnen <Niklas.kleku@icloud.com>
2023-02-03 13:42:45 +0000

    Bugfixes and refactoring

--- R/Seuratwrapper.R
+++ R/Seuratwrapper.R
@@ -1,11 +1,11 @@
-Seurat_to_Spaco <- function(Seurat,assay="SCT",n.image=1,slot="scale.data"){
+seurat_to_spaco <- function(Seurat, assay = "SCT", n_image = 1, slot = "scale.data") {
   data <- t(as.matrix(GetAssayData(object = Seurat, assay = DefaultAssay(Seurat), slot = "scale.data")))
-   if(nrow(data) == 0 & ncol(data) == 0){
+   if (nrow(data) == 0 && ncol(data) == 0 ) {
      stop("Assay to transform from Seurat is empty.")
    }
 
   data <- t(as.matrix(GetAssayData(object = Seurat, assay = DefaultAssay(Seurat), slot = "scale.data")))
-  tissue_positions_list <- as.data.frame(Seurat@images[[n.image]]@coordinates)
+  tissue_positions_list <- as.data.frame(Seurat@images[[n_image]]@coordinates)
 
   tissue_positions_list <- tissue_positions_list[tissue_positions_list$tissue == 1, c("row", "col")]
   distm <- as.matrix(dist(tissue_positions_list, method = "euclidean", upper = TRUE))
@@ -16,18 +16,17 @@ if (any(colSums(neighboursindex) == 0)) {
   warning("removing cells without any neighbours in defined distance")
   neighboursindex <- neighboursindex[colSums(neighboursindex) != 0, colSums(neighboursindex) != 0]
   data <- data[colnames(neighboursindex), ]
-  tissue_positions_list <- tissue_positions_list[rownames(data),]
+  tissue_positions_list <- tissue_positions_list[rownames(data), ]
 }
 
 LociNames <- colnames(neighboursindex)
-data <- data[match(LociNames, rownames(data)),]
+data <- data[match(LociNames, rownames(data)), ]
 
 return(SpaCoObject(neighbours <-  neighboursindex, data <-  as.matrix(data), coordinates <- tissue_positions_list))
 }
 
-Spaco_to_Seurat <- function(SpaCoObject,Seurat){
-  if(all(colnames(Seurat[[DefaultAssay(Seurat)]])%in%rownames(SpaCoObject@data))==FALSE)
-  {
+spaco_to_seurat <- function(SpaCoObject, Seurat ) {
+  if (all(colnames(Seurat[[DefaultAssay(Seurat)]] )%in% rownames(SpaCoObject@data)) == FALSE) {
     stop("Cells without neighbours in defined distance found in Seurat object. Please subset cells first.")
   }
 
@@ -36,7 +35,7 @@ Spaco_to_Seurat <- function(SpaCoObject,Seurat){
   return(Seurat)
 }
 
-subset_non_neighbour_cells <- function(SpaCoObject, Seurat){
+subset_non_neighbour_cells <- function(SpaCoObject, Seurat) {
   require(Seurat)
 
   Seurat <- subset(Seurat, cells = intersect(colnames(Seurat),rownames(SpaCoObject@data)))
