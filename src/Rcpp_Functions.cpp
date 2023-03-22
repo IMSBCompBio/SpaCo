8cf0c0b67d658acdee05d9adc2a66bd1e68f324d
NKleinenkuhnen <Niklas.kleku@icloud.com>
2023-03-22 15:15:52 +0000

    solving c++ compiling issues.

--- src/Rcpp_Functions.cpp
+++ src/Rcpp_Functions.cpp
@@ -1,4 +1,23 @@
-// [[Rcpp::depends(RcppArmadillo, RcppEigen)]]
+//' Multiply two matrices using Eigen library
+//'
+//' This function multiplies two matrices using the Eigen library, which provides
+//' fast linear algebra operations.
+//'
+//' @param A a matrix
+//' @param B a matrix
+//' @return the product of A and B
+//' @export
+//'
+//' @examples
+//' A <- matrix(1:6, nrow=2)
+//' B <- matrix(7:12, nrow=2)
+//' eigenMapMatMult(A, B)
+//'
+//' @importFrom Rcpp sourceCpp
+//' @useDynLib SPACO
+
+
+// [[Rcpp::depends(RcppEigen)]]
 
 #include <RcppEigen.h>
 #include <Eigen/Dense>
@@ -6,7 +25,7 @@
 
 // [[Rcpp::export]]
 SEXP eigenMapMatMult(const Eigen::Map<Eigen::MatrixXd> A, Eigen::Map<Eigen::MatrixXd> B){
-    Eigen::MatrixXd C = A * B;
+  Eigen::MatrixXd C = A * B;
 
-    return Rcpp::wrap(C);
+  return Rcpp::wrap(C);
 }
